0e50eccbe52bc222c6d5d2da8afca4a2caaa14f5
swap123 <misalswapnil77@gmail.com>
2022-03-04 16:43:03 +0530

    Player Reaches to Hundred

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -2,11 +2,47 @@ package com.bridgelabz;
 
 public class SnakeLadder {
     public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
         int position = 0;
-        System.out.println(" Player rolls a Diece");
+        System.out.println(" Welcome player!! ");
 
-        int randomv = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println(" Number Got: " + randomv);
+
+        int dice;
+        int option;
+        int roll = 0;
+
+
+        while (position < 100) {
+            dice = (int)Math.floor( (Math.random() * 10) % 6 + 1);
+            System.out.println("\n The dice was rolled to: " + dice);
+            option = (int)Math.floor(Math.random() * 3);
+
+            switch(option) {
+                case 0:
+                    System.out.println(" Player action: nothing");
+                    break;
+                case 1:
+                    System.out.println(" Player action: ladder");
+                    position += dice;
+                    if (position > 100) {
+                        position -= dice;
+                        System.out.println(" Exceeded 100 so not counting");
+                    }
+                    break;
+                case 2:
+                    System.out.println(" Player action: snake");
+                    position -= dice;
+                    position = position < 0 ? 0 : position;
+                    break;
+
+            }
+
+            System.out.println(" Current position: " + position);
+            roll++;
+        }
+
+        System.out.println(" Congratulations!! You took " + roll + " rolls to win the game!!");
     }
+
 }
